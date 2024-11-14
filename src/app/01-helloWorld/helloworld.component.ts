b01747142f69d7cb7c9e85230786504b8af76d8c
Rhayssa Kramer <112133224+rhayssakramer@users.noreply.github.com>
2024-11-13 21:45:56 -0300

    Criando o primeiro Hello World

---
+++ src/app/01-helloWorld/helloworld.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello-world',
+  standalone: true,
+  template: `
+  <p>Hello World!</p>
+  `
+})
+export class HelloWorldComponent { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiroComponent } from './02-primeiro-component/meu-primeiro.component';
+import { HelloWorldComponent } from './01-helloWorld/helloworld.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MeuPrimeiroComponent],
+  imports: [RouterOutlet, MeuPrimeiroComponent, HelloWorldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
